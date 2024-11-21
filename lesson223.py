a03c32704cd3e9ddfbe63c6416530a6a4de2a050
Mark Peters <mapeters101@earthlink.net>
2024-11-21 17:57:49 -0500

    Lesson223:  Work on lesson to 4:50.

---
+++ lesson223.py
@@ -0,0 +1,29 @@
+
+s1 = {1, 2, 3, 'a', 'b', 4}
+print(s1)
+print(s1)
+print(s1)
+print(s1)
+print(s1)
+#print(s1[0])
+print()
+
+s1 = {1, 2, 3, 'a', 'b', 4, 1, 2, 'a', 3, 10}
+print(s1)
+s1.add((10, 20))
+print(s1)
+s1.remove('a')
+print(s1)
+print()
+
+l1 = [1, 2]
+#s1.add(l1)
+
+s2 = set()
+print(s2)
+
+s3 = {}
+print(type(s3))
+
+
+
