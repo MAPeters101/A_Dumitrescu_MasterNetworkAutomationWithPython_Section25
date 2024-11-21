05c69f403bac8846d8e1d61250be1b3076fea0c8
Mark Peters <mapeters101@earthlink.net>
2024-11-21 18:06:04 -0500

    Lesson223:  Complete work on lesson from 4:50.

--- lesson223.py
+++ lesson223.py
@@ -24,6 +24,36 @@ print(s2)
 
 s3 = {}
 print(type(s3))
+print()
+
+s4 = set('helloooooo!!!')
+print(s4)
+
+s5 = set((1, 2, 3, 4, 'abc'))
+print(s5)
+
+l2 = [10, 20, 30, 40]
+print(set(l2))
+
+macs = ['30-24-32-E2-0F-59', '30-24-32-E2-0F-59', '30-24-32-E2-0F-59',
+        '30-24-32-E2-0F-59', '30-24-32-E2-0F-59', '30-24-32-E2-0F-60']
+mac_addresses = set(macs)
+print(mac_addresses)
+print(len(mac_addresses))
+print()
+
+mac_addresses = list(set(macs))
+print(mac_addresses)
+print(len(mac_addresses))
+print()
+
+
+for item in s4:
+    print(item)
+print()
+
+print('x' in s4)
+
 
 
 
