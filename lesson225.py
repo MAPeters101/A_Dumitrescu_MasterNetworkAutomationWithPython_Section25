b901fface44b4bca8d222bbf035090505b78b7c7
Mark Peters <mapeters101@earthlink.net>
2024-11-22 12:12:32 -0500

    Lesson225:  Complete work on lesson from 4:00.

--- lesson225.py
+++ lesson225.py
@@ -34,5 +34,39 @@ set4 = set2 - set1
 print(f'{set4=}')
 print()
 
+# 3. set.symmetric_difference()
+set5 = set1.symmetric_difference(set2)
+print(f'{set5=}')
+
+set4 = set1 ^ set2
+print(f'{set5=}')
+print()
+
+
+# 4. set.union()
+set6 = set1.union(set2)
+print(f'{set6=}')
+
+set6 = set1 | set2
+print(f'{set6=}')
+print()
+
+
+# 4. set.isdisjoint()
+s1 = {1, 3, 5}
+s2 = (5, 6, 7)
+print(s1.isdisjoint(s2))
+s3 = {8, 9}
+print(s1.isdisjoint(s3))
+print()
+
+# <, <=, >, >=
+print({1, 3} < {1, 2, 3, 4})
+print({1, 2, 3, 4} < {1, 2, 3, 4})
+print({1, 2, 3, 4} <= {1, 2, 3, 4})
+print({1, 2, 3, 4} >= {1, 2, 3, 4})
+print({1, 2, 3, 4} > {1, 2, 3, 4})
+print({1, 2, 3, 4, 5} >= {1, 2, 3, 4})
+print({1, 2, 3, 4, 5} > {1, 2, 3, 4})
 
 
